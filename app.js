34d3743e2e42b2597346c4ebd9164a18a3709cff
Ellmaer <ellmaer2003@gmail.com>
2024-04-11 11:04:46 -0700

    added index.html for testing

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //MAIN LOGIC, INITIALIZATION OF THE SERVER IS DONE HERE
+
+
+//TO RUN LOCALLY, TYPE "node app.js" IN TERMINAL (W/O THE QUOTATION MARKS)
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set the path to your public directory
+const publicDirectory = path.join(__dirname, 'public');
+
+// Serve static files from the public directory
+app.use(express.static(publicDirectory, { index: 'index.html' }));
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
