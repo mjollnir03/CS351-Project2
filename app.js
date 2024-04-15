baf23f0ea3bd64f8f996b5047cffbad9075c0c87
Ellmaer <ellmaer2003@gmail.com>
2024-04-15 12:01:28 -0700

    all files from project 1 uploaded

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Set the path to your public directory
 const publicDirectory = path.join(__dirname, 'public');
