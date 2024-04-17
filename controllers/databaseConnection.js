1f4945e9f3d1e93f6846ff0d0655532e971ce27e
Ellmaer <ellmaer2003@gmail.com>
2024-04-17 11:27:57 -0700

    updated the app.js

--- controllers/databaseConnection.js
+++ controllers/databaseConnection.js
@@ -1,7 +1,5 @@
 //THE FOLLOWING FILE WILL NOT BE PUSHED TO THE GITHUB REPO
 //THIS IS FOR PRIVACY REASONS
 
-
-
-
+// THE LINE BELOW WILL HAVE TO BE COMPLETELY CHANGED
 // module.exports = { uri: 'mongodb+srv://YOUR_Login:YOUR_Password@cluster0.WHATEVER.mongodb.net/?retryWrites=true&w=majority' };
