ea8d838b974a773ec6162a1ee6728dfc096b7b09
Ellmaer <ellmaer2003@gmail.com>
2024-04-18 12:58:07 -0700

    .gitignore updated to ignore the controller file

--- controllers/databaseConnection.js
+++ controllers/databaseConnection.js
@@ -4,3 +4,4 @@
 
 // THE LINE BELOW WILL HAVE TO BE COMPLETELY CHANGED (Privacy Reasons)
 module.exports = { uri: 'mongodb+srv://eranjber:f2KwQTC7BqrMsHRK@cluster0.anve4ux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' };
+
