eabe5d4b28cb4525084962451a1402d2e1cb1d5a
Ellmaer <ellmaer2003@gmail.com>
2024-04-18 12:53:07 -0700

    connection to mongodb database made

--- controllers/databaseConnection.js
+++ controllers/databaseConnection.js
@@ -1,5 +1,6 @@
 //THE FOLLOWING FILE WILL NOT BE PUSHED TO THE GITHUB REPO
 //THIS IS FOR PRIVACY REASONS
 
-// THE LINE BELOW WILL HAVE TO BE COMPLETELY CHANGED
+
+// THE LINE BELOW WILL HAVE TO BE COMPLETELY CHANGED (Privacy Reasons)
 // module.exports = { uri: 'mongodb+srv://YOUR_Login:YOUR_Password@cluster0.WHATEVER.mongodb.net/?retryWrites=true&w=majority' };
