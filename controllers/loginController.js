7c88a8591c8b1b94a5e4c62919815d72c81c7ed0
Falenheart <Austinly0@aim.com>
2024-04-23 09:54:30 -0700

    Deleted from function

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -37,9 +37,6 @@ async function lookUpAccount(email, password) {
         if (user) {
             console.log("User found:", user);
             console.log("Login successful");
-
-            // Assuming that we connect to the database here...
-            module.exports = user;
         } else {
             console.log("User not found or invalid credentials");
         }
