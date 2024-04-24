db11bea434353d85058a4daefbe57a7773e79cde
Falenheart <Austinly0@aim.com>
2024-04-23 18:28:17 -0700

    Cleaned up file

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,17 +1,65 @@
-const user = require("./loginController");
+const { uri} = require('./databaseConnection')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 
 const addToCart = async(product) => {
-    // Connected to user
-    // Add the product to user
+    try {
+        await client.connect();
+        // Connect to the right database
+        // const db = client.db('DATABASE NAME');
+
+        // Connect to the right collection
+        // const collection = db.collection('COLLECTION NAME');
+
+
+        // Inserting data into the collection
+        // collection.insertOne({product: product, quantity: 0, MAYBE SOMETHING RELATED TO THE USER});
+    }
+    finally {
+        await client.close();
+    }
 }
 
 const removeFromCart = async(product) => {
-    // Connect to user
-    // Remove the product from the user
+    try {
+        await client.connect();
+        // Connect to the right database
+        // const db = client.db('DATABASE NAME');
+
+        // Connect to the right collection
+        // const collection = db.collection('COLLECTION NAME');
+
+
+        // Remove data from collection
+    }
+    finally {
+        await client.close();
+    }
 }
 
 const updateCart = async(product, quantity) => {
+    try {
+        await client.connect();
+        // Connect to the right database
+        // const db = client.db('DATABASE NAME');
 
+        // Connect to the right collection
+        // const collection = db.collection('COLLECTION NAME');
+
+
+        // Update collection
+    }
+    finally {
+        await client.close();
+    }
 }
 
 module.exports = {
